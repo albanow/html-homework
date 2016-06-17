cf9506a17f6a46cb9f594629ec92d49093638f4c
Albano Pena <albanow10@gmail.com>
2016-06-17 02:00:33 -0500

    First commit, initial code

---
+++ public_html/js/scripts.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function(){
+
+    "use strict";
+    /*
+     ----------------------------------------------------------------------
+     Animation
+     ----------------------------------------------------------------------
+     */
+    $('.animated').appear(function() {
+        var elem = $(this);
+        var animation = elem.data('animation');
+        if ( !elem.hasClass('visible') ) {
+            var animationDelay = elem.data('animation-delay');
+            if ( animationDelay ) {
+                setTimeout(function(){
+                    elem.addClass( animation + " visible" );
+                }, animationDelay);
+            } else {
+                elem.addClass( animation + " visible" );
+            }
+        }
+    });
+
+
+
+}); // End $(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
